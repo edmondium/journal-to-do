f677b08c7d4361ac9cca4989ebe21e160ff7c0d3
edmondium <nicko@uin-suska.ac.id>
2022-05-28 20:57:10 +0700

    parallel on simulation 1

--- seri1.c
+++ seri1.c
@@ -35,6 +35,7 @@ int main()
 			#endif
 		}
 	}
+	#pragma omp parallel for
 	for (k=0; k<KE; k++)
 	{
 		ex[k]=0;
@@ -60,67 +61,62 @@ int main()
 		#else
 		start=clock();
 		#endif
-		#pragma omp parallel
-		{
-		#pragma omp single
+
 		for (n=1; n<=NSTEPS; n++)
 		{
 			T=T+1;
-			//#pragma omp simd
-			for (k=1; k<KE; k++)
+			pulse=exp(-.5*(pow((t0-T)/spread, 2.0)));
+			ex[kc]=pulse;
+
+			#pragma omp parallel for
+			for (k=0; k<KE; k++)
 			{
 				ex[k]=ex[k]+.5*(hy[k-1]-hy[k]);
 			}
-			pulse=exp(-.5*(pow((t0-T)/spread, 2.0)));
-			ex[kc]=pulse;
-			//#pragma omp simd
+
+			#pragma omp parallel for
 			for (k=0; k<KE-1; k++)
 			{
 				hy[k]=hy[k]+.5*(ex[k]-ex[k+1]);
 			}
 		}
-		}
+		
 		#ifdef _OPENMP
 		double endtime=omp_get_wtime();
 		#else
 		end=clock();
 		#endif
-	/*	printf("k\t ex[k]\t hy[k]\n"); 
-		for (k=1; k<=KE; k++)
-		{
-			printf("%3d | %f | %f\n", k, ex[k], hy[k]);
-		}*/
 		
 		ifp = fopen(inputFilename, "w");
 		fprintf(ifp, "# NSTEPS = %d\n", NSTEPS);
 		fprintf(ifp, "# t0-T = %5.0f | ex[kc] = %f\n", t0-T, ex[kc]);
+		#ifdef _OPENMP
+		fprintf(ifp, "# Time taken = %f second on %i threads.\n", endtime-starttime, numThreads);
+		#else
+		fprintf(ifp, "# Waktu eksekusi = %f detik pada %i thread.\n", ((double)end-(double)start)/CLOCKS_PER_SEC, numThreads);
+		#endif
 		fprintf(ifp, "# k\t ex[k]\n");
-		//#pragma omp parallel for
+		#pragma omp for
 		for (k=0; k<KE; k++)
 		{
 			fprintf(ifp, "%3d %f\n", k, ex[k]);
 		}
-		//#ifdef _OPENMP
-		//fprintf(ofp, "# Time taken = %f second on %i threads.\n", endtime-starttime, numThreads);
-		//#else		
-		fprintf(ifp, "# Waktu eksekusi = %f detik pada %i thread.\n", ((double)end-(double)start)/CLOCKS_PER_SEC, numThreads);
-		//#endif
 		fclose(ifp);
 
 		ofp = fopen(outputFilename, "w");
 		fprintf(ofp, "# NSTEPS = %d\n", NSTEPS);
 		fprintf(ofp, "# t0-T = %5.0f | ex[kc] = %f\n", t0-T, ex[kc]);
-		fprintf(ofp, "# k\t hy[k]\n");
-		//for (k=1; k<=KE; k++)
-		for (k=0; k<KE; k++)
-		{
-			fprintf(ofp, "%3d %f\n", k, hy[k]);
-		}
 		#ifdef _OPENMP
 		fprintf(ofp, "# Time taken = %f second on %i threads.\n", endtime-starttime, numThreads);
 		#else
 		fprintf(ofp, "# Waktu eksekusi = %f detik pada %i thread.\n", ((double)end-(double)start)/CLOCKS_PER_SEC, numThreads);
 		#endif
+		fprintf(ofp, "# k\t hy[k]\n");
+		#pragma omp for
+		for (k=0; k<KE; k++)
+		{
+			fprintf(ofp, "%3d %f\n", k, hy[k]);
+		}
 		fclose(ofp);
 
 		printf("T = %5.0f\n", T);
